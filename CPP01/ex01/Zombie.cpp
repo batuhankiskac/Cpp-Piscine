dfe38ab11a91128dfcfa9a5d0aa7451aa41a9845
batuhankiskac <kiskacbatuhan@gmail.com>
2025-06-05 20:36:10 +0300

    Implement Zombie class with constructor, destructor, and announce method; add Makefile for project setup

---
+++ CPP01/ex01/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) {
+	this->name = name;
+}
+
+Zombie::~Zombie() {
+	std::cout << "Zombie " << this->name << " is destroyed." << std::endl;
+}
+
+void Zombie::announce() const {
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
