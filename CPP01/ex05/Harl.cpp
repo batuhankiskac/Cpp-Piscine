c7c34f022649c42f6c2b1b59319ffc2a9b383b59
batuhankiskac <kiskacbatuhan@gmail.com>
2025-06-06 17:30:32 +0300

    Implement Harl class with debug, info, warning, and error methods; update main function and Makefile for project setup

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,33 @@
+#include "Harl.hpp"
+
+void Harl::debug(void) {
+	 std::cout << "[ DEBUG ]\n"
+			   << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
+
+void Harl::info(void) {
+	std::cout << "[ INFO ]\n"
+			  << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void Harl::warning(void) {
+	std::cout << "[ WARNING ]\n"
+			  << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error(void) {
+	std::cout << "[ ERROR ]\n"
+			  << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level) {
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*functions[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			(this->*functions[i])();
+			return;
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]\n";
+}
