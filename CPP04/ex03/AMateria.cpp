35b8832bbe25bceb0076108b92cad477124e9a56
batuhankiskac <kiskacbatuhan@gmail.com>
2025-08-03 14:41:59 +0300

    Implement AMateria class with constructors, assignment operator, and methods

---
+++ CPP04/ex03/AMateria.cpp
@@ -0,0 +1,23 @@
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+AMateria::AMateria(std::string const & type) : type(type) { }
+
+AMateria::AMateria(const AMateria &other) : type(other.type) { }
+
+AMateria &AMateria::operator=(const AMateria &other) {
+	if (this != &other) {
+		this->type = other.type;
+	}
+	return (*this);
+}
+
+AMateria::~AMateria() { }
+
+std::string const & AMateria::getType() const {
+	return (this->type);
+}
+
+void AMateria::use(ICharacter& target) {
+	(void)target;
+}
