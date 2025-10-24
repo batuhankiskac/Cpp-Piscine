2356907b991f9b4a5c180c790c726dd1291eb993
batuhankiskac <kiskacbatuhan@gmail.com>
2025-10-24 18:41:19 +0300

    Implement AForm and Bureaucrat classes with grade validation, exception handling, and basic functionality

---
+++ CPP05/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include <iostream>
+
+void printTitle(std::string title)
+{
+	std::cout << "\n" << "\033[1;36m" << "--- " << title << " ---" << "\033[0m" << std::endl;
+}
+
+int main() {
+
+
+	return (0);
+}
