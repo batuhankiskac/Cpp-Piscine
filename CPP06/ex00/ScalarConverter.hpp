4393584633dd97333d52c3a05e09210b0d901544
batuhankiskac <kiskacbatuhan@gmail.com>
2025-12-21 13:10:20 +0300

    Refactor ScalarConverter: remove type detection and print methods; update convert method signature

--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,16 +6,6 @@
 #include <cstdlib>
 #include <limits>
 #include <cmath>
-#include <iomanip>
-#include <cctype>
-
-enum e_type {
-	TOKEN_CHAR,
-	TOKEN_INT,
-	TOKEN_FLOAT,
-	TOKEN_DOUBLE,
-	TOKEN_INVALID
-};
 
 class ScalarConverter {
 private:
@@ -23,12 +13,6 @@ private:
 	ScalarConverter(const ScalarConverter &other) { (void)other; }
 	~ScalarConverter() { }
 	ScalarConverter& operator=(const ScalarConverter &other) { (void)other; return *this; }
-
-	static e_type detectType(const std::string& str);
-	static void printChar(char c);
-	static void printInt(int i);
-	static void printFloat(float f);
-	static void printDouble(double d);
 public:
 	static void convert(const std::string &str);
 };
