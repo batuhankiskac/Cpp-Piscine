339d57e95789a620cf277430c2cfd8ab33c0b4b5
Batuhan Kıskaç <kiskacbatuhan@gmail.com>
2026-03-27 15:44:21 +0300

    minor

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,11 +6,11 @@ int main() {
 	for (int i = 0; i < 10; ++i)
 		x.push_back(i);
 
-	std::vector<int>::iterator it = ::easyfind(x, 4);
+	std::vector<int>::iterator it = easyfind(x, 4);
 	if (it != x.end())
 		std::cout << "Value found!\n";
 
-	::easyfind(x, 14);
+	easyfind(x, 14);
 
 	return 0;
 }
